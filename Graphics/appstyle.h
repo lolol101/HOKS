799f0c349a91db2ca1be3a717dd9e2934a7e4580
Ilia Kholopkin <iliagold@icloud.com>
2023-04-01 23:10:07 +0300

    Added unorderd_map for colors

--- Graphics/appstyle.h
+++ Graphics/appstyle.h
@@ -1,53 +1,71 @@
 #ifndef APPSTYLE_H
 #define APPSTYLE_H
 #include <QString>
+#include <unordered_map>
+#include <string>
 
 class style_table {
 public:
+    std::unordered_map<std::string, QString> colors = {
+            {"main_window_background_color",                    "#111111;"},
+            {"button_standard_background_color",                "#111111;"},
+            {"button_standard_border_color",                    "#666666;"},
+            {"button_standard_color",                           "#ffffff;"},
+            {"button_standard_border_color_hover",              "#777777;"},
+            {"button_standard_background_color_pressed_hover",  "#777777;"},
+            {"button_standard_color_pressed_hover",             "#ffffff;"},
+            {"line_edit_standard_color",                        "#a4a4a4;"},
+            {"line_edit_standard_background_color",             "#333333;"},
+            {"label_standard_color",                            "#a4a4a4;"},
+            {"message_box_standard_background_color",           "#111111;"},
+            {"authorization_label_color",                       "#ffffff;"},
+            {"wrong_registration_data_label_color",             "#ff7373;"},
+    };
+
     QString main_window =
             "QMainWindow {"
-                "background-color: #111111;"
+                "background-color: " + colors["main_window_background_color"] +
             "}";
     QString button_standard =
             "QPushButton {"
-                "background-color: #111111;"
+                "background-color: " + colors["button_standard_background_color"] +
                 "border: 1px solid;"
                 "border-radius: 19px;"
-                "border-color: #666666;"
-                "color: #ffffff;"
+                "border-color: " + colors["button_standard_border_color"] +
+                "color: " + colors["button_standard_color"] +
                 "padding: 10px 10px;"
             "}"
             "QPushButton:hover {"
-                "border-color: #777777;"
+                "border-color: " + colors["button_standard_border_color_hover"] +
             "}"
             "QPushButton:pressed:hover {"
-                "background-color: #777777;"
-                "color: #ffffff;"
+                "background-color: " + colors["button_standard_background_color_pressed_hover"] +
+                "color: " + colors["button_standard_color_pressed_hover"] +
             "}";
     QString line_edit_standard =
             "QLineEdit {"
-                "color: #a4a4a4;"
-                "background-color: #333333;"
+                "color: " + colors["line_edit_standard_color"] +
+                "background-color: " + colors["line_edit_standard_background_color"] +
                 "border-radius: 12px;"
                 "padding: 6px;"
             "}";
     QString label_standard =
             "QLabel {"
-                "color: #a4a4a4;"
+                "color: " + colors["label_standard_color"] +
             "}";
     QString message_box_standard =
             "QMessageBox {"
-                "background-color: #111111;"
+                "background-color: " + colors["message_box_standard_background_color"] +
             "}";
     QString authorization_label =
             "font-family: Geneva;"
             "font-size: 24px;"
-            "color: #ffffff;"
+            "color: " + colors["authorization_label_color"] +
             "qproperty-alignment: AlignCenter;";
     QString left_alignment_label =
             "qproperty-alignment: AlignLeft;";
     QString wrong_registration_data_label =
-            "color: #ff7373;";
+            "color: " + colors["wrong_registration_data_label_color"];
 };
 
 #endif // APPSTYLE_H
--- Graphics/mainwindow.cpp
+++ Graphics/mainwindow.cpp
@@ -1,7 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <appstyle.h>
-#include <QDebug>
 #include <QMessageBox>
 #include <QString>
 #include <vector>
